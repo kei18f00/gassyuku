284aeb3b73753df6b3cb0c6edf4f7732f215d68f
kei18f00 <kei18f00@gmail.com>
2019-09-05 23:51:36 +0900

    BB_Hiranuma

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
 
-const { app, Menu, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow,ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 var schedule = require("node-schedule");
-let mainWindow;
+
+function setTime(arg){
+    let recordwindow
+    var job = schedule.scheduleJob(arg, function (){
+        console.log("開いたよ");
+        recordwindow = new BrowserWindow({ width: 800, height: 600, webPreferences:{
+            nodeIntegration: true
+        },title:"hitotume"});
+        const { closedayOfweek, closehour, closeminute, closesecond } = arg
+        var job2 = schedule.scheduleJob({
+            dayOfweek :closedayOfweek,
+            hour :closehour,
+            minute : closeminute,
+            second : closesecond,
+        }, function (){
+            recordwindow.close()
+        });
+        recordwindow.loadURL(arg.URL)
+        recordwindow.on('closed', () => {
+            mainrecordwindowWindow = null;
+        });
+    });
+}
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 800, height: 600, webPreferences:{
-        nodeIntegration: true
-    },title:"hitotume"});
-    subWindow = new BrowserWindow({ width: 800, height: 600, webPreferences:{
+
+    subWindow = new BrowserWindow({ width: 520, height: 600, webPreferences:{
         nodeIntegration: true
     } });
-        
-    var job = schedule.scheduleJob({
-        dayOfWeek :2,
-        hour :17,
-        minute :05,
-        second :00,
-    }, function (){
-        console.log("開いたよ");
-        mainWindow.loadURL("https://www.nhk.or.jp/radio/player/?ch=r1")
-        subWindow.loadURL(url.format({
-            pathname: path.join(__dirname, 'index.html'),
-            protocol: 'file:'
-        }));
-    });
-
-    var job = schedule.scheduleJob({
-        dayOfWeek :2,
-        hour :17,
-        minute :05,
-        second : 30,    }, function (){
-        mainWindow.close()
-    });
-    //mainWindow.loadURL("https://www.nhk.or.jp/radio/player/?ch=r1")
+    subWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'public/index.html'),
+        protocol: 'file:'
+    }));
+    ipcMain.on('set', (event, arg) => {
+        console.log(arg) //ここが送信した中身
+        setTime(arg)
+    })
 
     // 開発ツールを有効化
-     subWindow.webContents.openDevTools();
+    //subWindow.webContents.openDevTools();
 
     Menu.setApplicationMenu(null);
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
 }
 
 app.on('ready', createWindow);
